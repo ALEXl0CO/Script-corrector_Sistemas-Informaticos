84a03465f2a3ed89e99730a0f35dd7cb350539aa
ALEXl0CO <alexmartinezjimenez2005daw@gmail.com>
2024-06-12 18:42:23 +0200

    Script finalizado y markdown actualizado.

--- README.md
+++ README.md
@@ -1,2 +1,133 @@
-# Script-corrector_Sistemas-Informaticos
-Repositorio público de la creación del script que dará formato a las actividades de la asignatura de Sistemas Informáticos
+# Script corrector Sistemas Informaticos
+
+## Descripción
+Este script de bash formatea los ejercicios de scripts de la asignatura de Sistemas Informáticos. Todo con el fin de facilitar la corrección de los scripts al profesorado y evitar la ejecución de posibles scripts maliciosos.
+
+
+## Requerimientos
+- Ejecutar el script en un entorno seguro para evitar posibles errores inesperados. Por ejemplo, una máquina virtual Linux.
+- Tener `unzip` instalado en el sistema para poder descomprimir los zips.
+- Instalar una extensión y aplicar la configuración mencionada para aumentar la velocidad de corrección. (Opcional pero altamente recomendado).
+
+
+## Uso
+Para poder usar este script primero deberás descargar dicho script. Después, deberás guardarlo en un directorio junto con las carpetas contenedoras de los zips.
+
+Debería tener el siguiente aspecto:
+IMAGEN
+
+Una vez guardado en un archivo con las carpetas conetnedoras de los zips, deberás darle permisos de ejecución:
+```bash
+sudo chmod +x corrector.sh
+```
+
+Después de haber otorgado los permisos necesarios, ejecuta el script:
+```bash
+bash script.sh
+```
+
+
+## Funcionamiento
+Este script tiene distintos menús en función de la cantidad de carpetas que hayan en el directorio con zips. Si hay más de una carpeta, se mostrará un menú con las carpetas disponibles para corregir. Si solo hay una carpeta, te mostrará sus respectivas opciones.
+
+Una vez ejecutado el script, no habrá problema en ejecutar de nuevo dicho script con el directorio de Respuestas ya que este está configurado para no aparecer.
+
+**¡IMPORTANTE**: Cada vez que se ejecute dicho script, sobreescibirá los archivos de la carpeta Respuestas. Por lo que si quieres conservar los resultados de una ejecución anterior, deberás moverlos a otra carpeta.
+
+### Menú para distintas carpetas
+IMAGEN
+
+### Menú para una sola carpeta
+IMAGEN
+
+
+## Resultado
+Una vez ejecutado el formateo, se creará un árbol de directorios en el cual se guardarán los directorios y ficheros de cada zip y todos archivos con cada script formateado para su posterior corrección. Adjunto el arbol de directorios de un ejemplo de ejecución:
+
+```
+.
+├── 1º DAW - B
+│   ├── Carmen_Hernández.zip
+│   ├── Laura_Fernández.zip
+│   ├── Luis_Martínez.zip
+│   ├── Miguel_Rodríguez.zip
+│   └── Pedro_López.zip
+├── Respuestas
+│   ├── CarpetasDescomprimidas
+│   │   └── 1º DAW - B
+│   │       ├── Carmen_Hernández
+│   │       │   ├── script1.sh
+│   │       │   ├── script2.sh
+│   │       │   ├── script3.sh
+│   │       │   ├── script4.sh
+│   │       │   ├── script5.sh
+│   │       │   ├── script6.sh
+│   │       │   ├── script7.sh
+│   │       │   ├── script8.sh
+│   │       │   └── script9.sh
+│   │       ├── Laura_Fernández
+│   │       │   ├── script1.sh
+│   │       │   ├── script2.sh
+│   │       │   ├── script3.sh
+│   │       │   ├── script4.sh
+│   │       │   ├── script5.sh
+│   │       │   ├── script6.sh
+│   │       │   ├── script7.sh
+│   │       │   ├── script8.sh
+│   │       │   └── script9.sh
+│   │       ├── Luis_Martínez
+│   │       │   ├── script1.sh
+│   │       │   ├── script2.sh
+│   │       │   ├── script3.sh
+│   │       │   ├── script4.sh
+│   │       │   ├── script5.sh
+│   │       │   ├── script6.sh
+│   │       │   ├── script7.sh
+│   │       │   ├── script8.sh
+│   │       │   └── script9.sh
+│   │       ├── Miguel_Rodríguez
+│   │       │   ├── script1.sh
+│   │       │   ├── script2.sh
+│   │       │   ├── script3.sh
+│   │       │   ├── script4.sh
+│   │       │   ├── script5.sh
+│   │       │   ├── script6.sh
+│   │       │   ├── script7.sh
+│   │       │   ├── script8.sh
+│   │       │   └── script9.sh
+│   │       └── Pedro_López
+│   │           ├── script1.sh
+│   │           ├── script2.sh
+│   │           ├── script3.sh
+│   │           ├── script4.sh
+│   │           ├── script5.sh
+│   │           ├── script6.sh
+│   │           ├── script7.sh
+│   │           ├── script8.sh
+│   │           └── script9.sh
+│   └── Resultados
+│       └── 1º DAW - B
+│           ├── Carmen_Hernández.md
+│           ├── Laura_Fernández.md
+│           ├── Luis_Martínez.md
+│           ├── Miguel_Rodríguez.md
+│           └── Pedro_López.md
+└── script.sh
+
+11 directories, 56 files
+```
+
+
+<br>
+
+## Configuración de la extensión del navegador
+Para aumentar la velocidad de corrección, se recomienda instalar la extensión de navegador [**Markdown Viewer**](https://chromewebstore.google.com/detail/ckkdlimhmcjmikdlpkmbgfkaikojcbjk) en el navegador Google Chrome. Esta extensión simplemente permite visualizar los archivos `.md` en el navegador sin necesidad de abrir un editor de texto.
+
+Una vez instalada, para configurar la extensión deberás seguir los siguientes pasos:
+1. Haz click izquierdo en el icono de la extensión.
+2. Clica en "ADVANCED OPTIONS".
+3. Dale permisos a la extensión para que pueda leer archivos locales, para ello clica en "ALLOW ACCESS" de la opción "File Access". Una vez dentro busca esta opción y actívala.
+   IMAGEN
+4. Finalmente, abre la configuración de la extensión de nuevo y en la barra de navegación de la parte superior clica en "Settings".
+5. En el apartado de "Theme" pon estas opciones:
+   IMAGEN
--- script.sh
+++ script.sh
@@ -1,16 +1,22 @@
-#/bin/bash
+#!/bin/bash
 
 ############ Menús dinámicos ############
 
-# Variable para comprobar si el menú es de tamaño inferior.
+# Variable global para comprobar si el menú será dinámico o no.
 opSeleccionada=0
+
 # Variable global que almacenará todas las carpetas con zips.
 carpetasConZips=()
+
+# Variable global que almacenará un valor u otro en caso de que el menú sea dinámico o no y la selección del usuario.
+#  Esto serivrá para no repetir código en caso de que el usuario quiera formatear todas las carpetas/clases, se
+#  usará el mismo método que para formatear una clase pero para cáda carpeta/clase dentro del directorio actual.
+textoUnicaClase=0
 
 ## Método que muestra el menú para formatear una clase.
 menuOpcionUnaClase() {
 
-    echo -e "\n\n\e[1m¿Qué quieres hacer?\e[0m\n"
+    echo -e "\n\e[1m¿Qué quieres hacer?\e[0m\n"
     echo "  1. Formatear clase"
     echo "  2. Salir"
 
@@ -21,10 +27,10 @@ menuOpcionUnaClase() {
 menuCompleto() {
 
     # Todas las opciones del menú.
-    opciones=("Elegir una clase" "Todas las clases" "Salir")
+    opciones=("Elegir una clase" "Todas las clases con zips" "Salir")
     contador=0
 
-    echo -e "\n\n\e[1m¿Qué quieres hacer?\e[0m\n"
+    echo -e "\n\e[1m¿Qué quieres hacer?\e[0m\n"
     for opcion in "${opciones[@]}"
     do
         contador=$((contador+1))
@@ -46,10 +52,19 @@ opcionSeleccionadaUsuario() {
         seleccion=-1
     fi
 
+    # Configuración necesaria para mejorar la usabilidad.
+    if [ $opSeleccionada -eq 0 -a $seleccion -eq 1 ]
+    then
+        textoUnicaClase=1
+    else
+        textoUnicaClase=0
+    fi
+
+
     # Switch de las opciones.
     case $seleccion in
         1)
-            formatearUnaCarpeta
+            formatearUnaCarpetaVisual
         ;;
         2)
             formatearTodasLasCarpetas
@@ -71,8 +86,17 @@ opcionSeleccionadaUsuario() {
 ##  archivos .zip o no en su interior para poder realizar el formateo.
 mostrarCarpetasYContenido() {
     
+    # Limpia el array de carpetas con zips para evitar errores.
+    carpetasConZips=()
+
     # Comprueba si hay carpetas en el directorio actual.
-    cantidadDirectorios=$(find -type d | cut -d "/" -f "2" | uniq | sort -n | tail -n +2 | wc -l)
+    cantidadDirectorios=$(find . -type d | cut -d "/" -f "2" | uniq | sort -n | tail -n +2 | wc -l)
+
+    # Comprueba si hay una carpeta de respuestas en el directorio actual para excluirla del formateador.
+    if [ -d "Respuestas" ]
+    then
+        cantidadDirectorios=$((cantidadDirectorios-1))
+    fi
 
     if [ $cantidadDirectorios -le '0' ]
     then
@@ -88,7 +112,7 @@ mostrarCarpetasYContenido() {
         # Bucle que recorre las carpetas y comprueba si tienen zips dentro o no.
         for carpeta in *
         do
-            if [ -d "$carpeta" ]
+            if [ -d "$carpeta" -a "$carpeta" != "Respuestas" ]
             then
                 # Variable que almacena la cantidad de archivos .zip del directorio seleccionado.
                 cantidadZipsDirectorio=$(find "$carpeta" -type f -name "*.zip" | wc -l)
@@ -110,14 +134,18 @@ mostrarCarpetasYContenido() {
             fi
         done
 
-        # Aquí se comprueba si todas las carpetas contienen archivos .zip o no y llama al método correspondiente. 
-        if [ $directoriosConZips -eq $cantidadDirectorios ]
-        then
-            menuCompleto
-        elif [ $directoriosConZips -eq 0 ]
+        # Si no contiene carpetas con zips, se mostrará un mensaje y se saldrá del script.
+        if [ $directoriosConZips -eq 0 ]
         then
             echo -e "\nLas carpetas están vacías, añade .zips para poder formatear."
             exit 0
+        fi
+
+        # En caso de tener, se comprueba si son varias carpetas las que hay y llama al método correspondiente.
+        echo -e "\n\n\e[1;33m¡ATENCIÓN! la ejeccución de este script eliminará, en caso de haber, correcciones previas.\e[0m"
+        if [ $directoriosConZips -gt 1 ]
+        then
+            menuCompleto
         else
             menuOpcionUnaClase
         fi
@@ -125,16 +153,187 @@ mostrarCarpetasYContenido() {
 }
 
 
-## Por implementar.
-formatearUnaCarpeta() {
+## Método que formatea una carpeta específica.
+formatearUnaCarpetaVisual() {
+    
+    clear
+    
+    # Si el menú está completo quiere decir que hay más de una carpeta y el usuario deberá poder elegirla.
+    #  En caso de ser un menú dinámico, ya que sólo hay una carpeta disponible para formatear, se formateará directamente.
+    #  Esto también sirve para no repetir código y llamar tantas veces como carpetas haya en el directorio actual si el menú
+    #  no es dinámico (hay muchas carpetas y se selecciona la opción "Todas las clases").
+    if [ $textoUnicaClase -eq 1 ]
+    then
+
+        selected=0
+        ESC=$(printf "\033")
+
+        # Bucle que lee las teclas de flechas y Enter del tecaldo.
+        while true
+        do
+            # Se limpia todo y se muestra el menú de nuevo con los cambios.
+            clear
+            echo -e "Selecciona una carpeta para formatearla \e[1m(Usa las flechas y la tecla 'Enter')\e[0m:\n"
+
+            # Bucle que muestra las opciones para seleccionar.
+            for i in "${!carpetasConZips[@]}"
+            do
+                if [ $i -eq $selected ]
+                then
+                    echo -e "\e[1;36m${carpetasConZips[i]}\e[0m"
+                else
+                    echo "${carpetasConZips[i]}"
+                fi
+            done
+
+            # Aquí se lee la entrada del teclado.
+            read -rsn1 key
+            if [[ $key == $ESC ]]
+            then
+                read -rsn2 key
+
+                # En caso de ser la tecla hacia arriba, se seleccionará la opción anterior.
+                if [[ $key == "[A" ]]
+                then
+                    selected=$((selected - 1))
+
+                    # Si estás en la primera opción y pulsas hacia arriba, bajará a la última.
+                    if [ $selected -lt 0 ]
+                    then
+                        selected=$((${#carpetasConZips[@]} - 1))
+                    fi
+
+                # En caso de ser la tecla hacia abajo, se seleccionará la opción posterior.
+                elif [[ $key == "[B" ]]
+                then
+                    selected=$((selected + 1))
+
+                    # Al igual que antes, si estás en la última opción y pulsas hacia abajo, subirá a la primera.
+                    if [ $selected -ge ${#carpetasConZips[@]} ]
+                    then
+                        selected=0
+                    fi
+                fi
+            
+            # Si se pulsa la tecla 'Enter', se mostrarán todas las opciones y la carpeta seleccionada resaltada.
+            elif [[ $key == "" ]]
+            then
+                clear
+                echo -e "Selecciona una carpeta para formatearla \e[1m(Usa las flechas y la tecla 'Enter')\e[0m:\n"
+                
+                for i in "${!carpetasConZips[@]}"
+                do
+                    if [ $i -eq $selected ]
+                    then
+                        echo -e "\e[1;32m${carpetasConZips[i]}\e[0m"
+                    else
+                        echo "${carpetasConZips[i]}"
+                    fi
+                done
+
+                echo -e "\nHas seleccionado: \033[1;32m${carpetasConZips[selected]}\033[0m\n"
+                formatearUnaCarpetaCodigo "${carpetasConZips[selected]}"
+                break
+            fi
+        done
+    else
+        clear
+        echo -e "Todas las carpetas se formatearán.\n"
+        formatearUnaCarpetaCodigo "${carpetasConZips[@]}"
+    fi
+}
+
+## Método pequeñito que complementa al métdo de formatear una carpeta visual general. Este se encarga de la parte del código.
+formatearUnaCarpetaCodigo() {
+    
+    # Método que comprueba si existe o no la carpeta de respuestas.
+    comprobarCarpetaRespuestas
+
+    # Variable que guarda todas las carpetas con zips que se le pasan como parámetro.
+    carpetas=("$@")
+
+    echo -e "Formateando la/s carpeta/s:"
+
+    for carpeta in "${carpetas[@]}"
+    do
+        echo -e "  \e[1;36m- $carpeta\e[0m"
+        accesoCarpetaParámetro "$carpeta"
+    done
+
+    tput cup 31 0
+    read -n1 -p "Pulsa cualquier tecla para continuar: "
     clear
 }
 
-## Por implementar.
+## Método que complementa al método general. Este pasa como parámetro todas las carpetas con zips para formatearlas.
 formatearTodasLasCarpetas() {
-    clear
+    formatearUnaCarpetaVisual "${carpetasConZips[@]}"
 }
 
+## Método encargado de comprobar la carpeta de respuestas.
+comprobarCarpetaRespuestas() {
+    
+    # Comprueba si existe o no la carpeta de respuestas. Si no existe la crea.
+    if [ ! -d "Respuestas" ]
+    then
+        mkdir Respuestas
+        mkdir Respuestas/CarpetasDescomprimidas
+        mkdir Respuestas/Resultados
+    fi
+}
+
+## Método que accede a la carpeta pasada como parámetro, descomprime cada zip, accede a la dicha carpeta y formatea los archivos.
+accesoCarpetaParámetro() {
+    
+    # Variable que guarda la carpeta pasada como parámetro.
+    carpeta="$1"
+
+    # Se crea la carpeta del alumno en el directorio de respuestas en caso de que no exista.
+    if [ ! -d "Respuestas/Resultados/$carpeta" ]
+    then
+        mkdir "Respuestas/Resultados/$carpeta"
+    fi
+
+    # Bucle que recorre los zips de la carpeta y los descomprime.
+    for zip in "$carpeta"/*.zip
+    do
+        # Extrae los zips en la carpeta de respuestas sin mostrar nada por pantalla.
+        unzip -o "$zip" -d "Respuestas/CarpetasDescomprimidas/$carpeta/" > /dev/null 2>&1
+    done
+
+    # Bucle que recorre los archivos de la carpeta y los formatea.
+    for carpetaAlumno in "Respuestas/CarpetasDescomprimidas/$carpeta"/*
+    do
+        if [ -d "$carpetaAlumno" ]
+        then
+            # Se obtiene el nombre del alumno.
+            nombreCompletoAlumnoRAW=$(basename "$carpetaAlumno")
+            nombreAlumno=$(echo "${nombreCompletoAlumnoRAW^}" | cut -d "_" -f 1)
+            apellidoAlumno=$(echo "${nombreCompletoAlumnoRAW^}" | cut -d "_" -f 2)
+            
+            # Creación de archivo Markdown para cada alumno en la ruta especificada.
+            markdownFile="Respuestas/Resultados/$carpeta/$nombreCompletoAlumnoRAW.md"
+            echo "# Scripts de $nombreAlumno $apellidoAlumno." > "$markdownFile"
+
+            # Bucle que recorre los archivos de la carpeta del alumno y añade al markdown.
+            for archivo in $(ls "$carpetaAlumno" | sort -V)
+            do
+                archivoCompleto="$carpetaAlumno/$archivo"
+
+                if [ -s "$archivoCompleto" ]
+                then
+                    echo -e "\n## $archivo" >> "$markdownFile"
+                    echo -e "\n\`\`\`bash" >> "$markdownFile"
+                    cat "$archivoCompleto" >> "$markdownFile"
+                    echo -e "\n\`\`\`" >> "$markdownFile"
+                else
+                    echo -e "\n## $archivo" >> "$markdownFile"
+                    echo "**¡SCRIPT NO REALIZADO!**" >> "$markdownFile"
+                fi
+            done
+        fi
+    done
+}
 
 
 ############ Inicio del programa ############
